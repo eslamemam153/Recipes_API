825104094745eb4b347a3145c60492ea34fe37c6
eslamemam153 <eslammostafaahmed23200@gmail.com>
2024-09-04 23:31:12 +0200

    first commit

---
+++ Js/main.js
@@ -0,0 +1,81 @@
+//Instant
+let myhttp = new XMLHttpRequest;
+let row = document.querySelector(".row")
+let input = document.querySelector("input")
+let selectedFood = document.querySelector("select")
+
+input.addEventListener("blur", function () {
+    getData(this.value)
+})
+selectedFood.addEventListener("change", function () {
+    getData(this.value)
+})
+
+//invoke_getData_Function
+getData(`cinnamon`)
+
+//Funtion_to_Get_Data
+function getData(data) {
+    myhttp.open("GET", `https://forkify-api.herokuapp.com/api/search?q=${data}`)
+    myhttp.send()
+    myhttp.addEventListener("readystatechange", function () {
+        if (myhttp.readyState == 4) {
+            let allData = JSON.parse(myhttp.response);
+            showData(allData.recipes)
+        }
+    })
+}
+
+//Funtion_to_Show_Data
+function showData(data) {
+    let box = ``
+    for (let i = 0; i < data.length; i++) {
+        box += `
+         <div class="col-md-4">
+        <img class="w-100 mb-2" src=" ${data[i].image_url}" alt="">
+        <p><b>Title: </b>${data[i].title}</p>
+        <p><b>Recipe Id: </b>${data[i].recipe_id}</p>
+        <p><b>Puplisher: </b>${data[i].publisher}</p>
+        <p><b>Social Rank: </b><span class="text-warning">${data[i].social_rank}</span></p>
+       </div> `
+
+    }
+    row.innerHTML = box
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
